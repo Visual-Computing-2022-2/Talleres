a381bf0c9ad3ae5c7558185df4067cd4fc9c3bc3
Jaider Andrés Pinto Pinto <jaider.pinto@hotmail.com>
2022-11-28 11:31:39 -0500

    Mas avance

--- content/sketches/shaders/lighting/mixShading.js
+++ content/sketches/shaders/lighting/mixShading.js
@@ -2,20 +2,19 @@
 
 let easycam;
 let objects;
-let toonShader;
-let ambient, ambient4;
-
+let mixShader;
+let ambient;
+let ambient4;
 function preload() {
-  toonShader = readShader("/Talleres/sketches/shaders/lighting/toon.frag", {
-    varyings: Tree.normal3,
+  mixShader = readShader("/Talleres/sketches/shaders/lighting/mix.frag", {
+    varyings: Tree.normal3 | Tree.position4,
   });
 }
 
 function setup() {
   createCanvas(600, 600, WEBGL);
+  noLights();
   colorMode(RGB, 1);
-  textureMode(NORMAL);
-  noStroke();
   setAttributes("antialias", true);
   let easyCamState = {
     distance: 325,
@@ -43,40 +42,61 @@ function setup() {
       color: color(random(), random(), random()),
     });
   }
-  ambient = createSlider(-1, 1, -0.4, 0.05);
+  shader(mixShader);
+  ambient = createSlider(0, 1, 0.2, 0.05);
   ambient.position(20, 10);
-  shader(toonShader);
-  toonShader.setUniform("ambient", ambient.value());
+  ambient.input(() => {
+    mixShader.setUniform("ambient", ambient.value());
+  });
   ambient4 = createColorPicker("white");
   ambient4.style("width", "60px");
   ambient4.position(20, 45);
   ambient4.input(() => {
     let ambient4Color = ambient4.color();
-    toonShader.setUniform("ambient4", [
+    mixShader.setUniform("ambient4", [
       red(ambient4Color) / 255,
       green(ambient4Color) / 255,
       blue(ambient4Color) / 255,
       1,
     ]);
   });
-  toonShader.setUniform("ambient4", [1, 1, 1, 1]);
+  mixShader.setUniform("ambient4", [1, 1, 1, 1]);
+  mixShader.setUniform("ambient", ambient.value());
 }
 
 function draw() {
   background(0);
+  let pointLight = getPointLight();
+  resetShader();
   let dirX = (mouseX / width - 0.5) * 2;
   let dirY = (mouseY / height - 0.5) * 2;
-  toonShader.setUniform(
+  mixShader.setUniform(
     "lightNormal",
     createVector(-dirX, -dirY, ambient.value()).array()
   );
+
   push();
   stroke("green");
   axes();
   grid();
   pop();
+  push();
+  translate(pointLight.position);
+  noStroke();
+  fill("white");
+  sphere(3);
+  pop();
+  shader(mixShader);
+  mixShader.setUniform(
+    "uLightPosition",
+    treeLocation(pointLight.position, {
+      from: Tree.WORLD,
+      to: Tree.EYE,
+    }).array()
+  );
   for (let i = 0; i < objects.length; i++) {
     push();
+    noStroke();
     fill(objects[i].color);
     translate(objects[i].position);
     rotate(objects[i].angle, objects[i].axis);
@@ -94,4 +114,16 @@ function draw() {
     }
     pop();
   }
+}
+
+function getPointLight() {
+  let angle = frameCount * 0.03;
+  let rad = 30;
+  let px = cos(angle) * rad;
+  let py = sin(angle) * rad;
+  let r = sin(angle) * 0.5 + 0.5;
+  return {
+    position: createVector(px, py, 0),
+    color: color(1 - r, r / 2, r),
+  };
 }
